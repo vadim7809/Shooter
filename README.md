339cb9031d73bb81e04cc048f9294f790913c9a5
Visslyx <romanpavelko70@gmail.com>
2025-06-08 16:26:25 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
-# Shooter
+import pygame
+
+import random
+
+pygame.init()
+
+
+window = pygame.display.set_mode([800, 600])
+clock = pygame.time.Clock()
+
+
+background_img = pygame.image.load("photo/galaxy (1).jpg")
+
+
+class Rocket():
+    def __init__(self, x, y, texture, speed, w, h):
+        self.texture = pygame.image.load(texture)
+        self.texture = pygame.transform.scale(self.texture, [w, h])
+        self.hitbox = self.texture.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+        self.speed = speed
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and self.hitbox.left > 0:
+            self.hitbox.x -= self.speed
+        if keys[pygame.K_RIGHT] and self.hitbox.right < 800:
+            self.hitbox.x += self.speed
+
+    def draw(self, window):
+        window.blit(self.texture, self.hitbox)
+
+class Enemy():
+    def __init__(self, x, y, texture, speed, w, h):
+        self.texture = pygame.image.load(texture)
+        self.texture = pygame.transform.scale(self.texture, [w, h])
+        self.hitbox = self.texture.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+        self.speed = speed
+
+    def move(self):
+        self.hitbox.y += self.speed
+
+    def draw(self, window):
+        window.blit(self.texture, self.hitbox)
+
+
+rocket = Rocket(350, 500, "photo/rocket (1).png", speed=5, w=80, h=100)
+
+
+enemies = []
+
+
+SPAWN_EVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(SPAWN_EVENT, 1000)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+
+        if event.type == SPAWN_EVENT:
+            x = random.randint(0, 730)
+            enemy = Enemy(x, 0, "photo/ufo (1).png", speed=2, w=70, h=70)
+            enemies.append(enemy)
+
+
+    rocket.move()
+
+
+    window.blit(background_img, (0, 0))
+
+
+    for enemy in enemies:
+        enemy.move()
+        enemy.draw(window)
+
+
+        if rocket.hitbox.colliderect(enemy.hitbox):
+            enemies.remove(enemy)
+
+
+    rocket.draw(window)
+
+    pygame.display.flip()
+    clock.tick(60)
--- main.py
+++ main.py
@@ -1,93 +1,123 @@
+import random
+
 import pygame
-import sys
-import random  # імпорт для рандому
+
+class Player:
+    def __init__(self, speed,
+                 x, y,
+                 width, height,
+                 skin):
+        self.speed = speed
+        self.skin = pygame.image.load(skin)
+        self.skin = pygame.transform.scale(self.skin, [width, height])
+        self.hitbox = self.skin.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+        self.bullets = []
+    def draw(self, window):
+        window.blit(self.skin, self.hitbox)
+        for bullet in self.bullets:
+            bullet.draw(window)
+
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d] and self.hitbox.x < 650:
+            self.hitbox.x += self.speed
+        if keys[pygame.K_a] and self.hitbox.x > 0:
+            self.hitbox.x -= self.speed
+        if keys[pygame.K_SPACE]:
+            self.bullets.append(Bullet(10, self.hitbox.x + 10, self.hitbox.y, 10, 20, "photo/bullet (1).png"))
+
+        for bullet in self.bullets:
+            bullet.update()
+
+
+class Enemy:
+    def __init__(self, speed,
+                 x, y,
+                 width, height,
+                 skin):
+        self.speed = speed
+        self.skin = pygame.image.load(skin)
+        self.skin = pygame.transform.scale(self.skin, [width, height])
+        self.hitbox = self.skin.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+    def draw(self, window):
+        window.blit(self.skin, self.hitbox)
+
+    def update(self):
+        self.hitbox.y += self.speed
+        if self.hitbox.y > 500:
+            self.hitbox.y = -100
+            self.hitbox.x = random.randint(0, 600)
+
+class Bullet:
+    def __init__(self, speed,
+                 x, y,
+                 width, height,
+                 skin):
+        self.speed = speed
+        self.skin = pygame.image.load(skin)
+        self.skin = pygame.transform.scale(self.skin, [width, height])
+        self.hitbox = self.skin.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+        self.bullets = []
+
+    def draw(self, window):
+        window.blit(self.skin, self.hitbox)
+
+    def update(self):
+        self.hitbox.y -= self.speed
 
 pygame.init()
-
-# Вікно
-window = pygame.display.set_mode([800, 600])
-pygame.display.set_caption("Rocket vs Enemies")
+window = pygame.display.set_mode([700, 500])
 clock = pygame.time.Clock()
 
-# Фон
-background_img = pygame.image.load("photo/backgr.png")
+background_img = pygame.image.load("photo/galaxy (1).jpg")
+background_img = pygame.transform.scale(background_img, [700, 500])
+game = True
 
-# Клас ракети
-class Rocket():
-    def __init__(self, x, y, texture, speed, w, h):
-        self.texture = pygame.image.load(texture)
-        self.texture = pygame.transform.scale(self.texture, [w, h])
-        self.hitbox = self.texture.get_rect()
-        self.hitbox.x = x
-        self.hitbox.y = y
-        self.speed = speed
+hero = Player(7, 500, 400, 50, 50, "photo/rocket (1).png")
 
-    def move(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and self.hitbox.left > 0:
-            self.hitbox.x -= self.speed
-        if keys[pygame.K_RIGHT] and self.hitbox.right < 800:
-            self.hitbox.x += self.speed
-
-    def draw(self, window):
-        window.blit(self.texture, self.hitbox)
-
-# Клас ворога
-class Enemy():
-    def __init__(self, x, y, texture, speed, w, h):
-        self.texture = pygame.image.load(texture)
-        self.texture = pygame.transform.scale(self.texture, [w, h])
-        self.hitbox = self.texture.get_rect()
-        self.hitbox.x = x
-        self.hitbox.y = y
-        self.speed = speed
-
-    def move(self):
-        self.hitbox.y += self.speed
-
-    def draw(self, window):
-        window.blit(self.texture, self.hitbox)
-
-# Ракета
-rocket = Rocket(350, 500, "photo/rocket.png", speed=5, w=80, h=100)
-
-# Список ворогів
 enemies = []
+y =  50
+for i in range(10):
+    enemies.append(Enemy(3, random.randint(0, 600), y, 50, 50, "photo/ufo (1).png"))
+    y -= 100
 
-# Таймер появи ворога (мілісекунди)
-SPAWN_EVENT = pygame.USEREVENT + 1
-pygame.time.set_timer(SPAWN_EVENT, 1000)  # кожну 1 секунду
-
-# Основний цикл
-while True:
+while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:  #
+            print(pygame.mouse.get_pos())  #
 
-        # Додаємо ворога
-        if event.type == SPAWN_EVENT:
-            x = random.randint(0, 730)  # Випадкове X (ширина екрану - ворог ~70px)
-            enemy = Enemy(x, 0, "photo/enemy1.png", speed=2, w=70, h=70)
-            enemies.append(enemy)
 
-    # Рух ракети
-    rocket.move()
 
-    # Фон
-    window.blit(background_img, (0, 0))
+    for bullet in hero.bullets[:]:
+        for enemy in enemies:
+            if bullet.hitbox.colliderect(enemy.hitbox):
+                hero.bullets.remove(bullet)
+                enemy.hitbox.y = -100
+                enemy.hitbox.x = random.randint(0, 600)
+                break
 
-    # Вороги
-    for enemy in enemies[:]:  # копія списку
-        enemy.move()
+
+    hero.update()
+
+    window.fill([123, 123,123 ])
+    window.blit(background_img, [0,0])
+
+    hero.draw(window)
+
+    for enemy in enemies:
         enemy.draw(window)
+        enemy.update()
 
-        # Зіткнення
-        if rocket.hitbox.colliderect(enemy.hitbox):
-            enemies.remove(enemy)
-
-    # Ракета
-    rocket.draw(window)
 
     pygame.display.flip()
+
     clock.tick(60)

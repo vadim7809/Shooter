c3fbea7ea01d8d65ab24ed8da663fb4ce6f42136
vadim7809 <mandyuk.vadim@gmail.com>
2025-06-25 10:57:59 +0300

    py

--- menu.py
+++ menu.py
@@ -1,21 +1,23 @@
+from PyQt6.QtWidgets import *
+from main import *
+from shop import *
 
+app = QApplication([])
+window = QWidget()
+stat_btn = QPushButton("Почати")
+shop_btn = QPushButton("Магазин")
+exit_btn = QPushButton("Вийти")
 
-from PyQt6.QtWidgets import*
-
-from main import window
-
-start_btn = QPushButton("Почати")
-shop_btn = QPushButton("shop")
-
-main_line =QVBoxLayout()
-main_line.addWidget(start_btn)
+main_line = QVBoxLayout()
+main_line.addWidget(stat_btn)
 main_line.addWidget(shop_btn)
+main_line.addWidget(exit_btn)
 
 window.setLayout(main_line)
 
-start_btn.clicked.connect(start_game)
+stat_btn.clicked.connect(start_game)
 shop_btn.clicked.connect(shop_window)
 
 
 window.show()
-window.exec()
+app.exec()
--- shop.py
+++ shop.py
@@ -1,34 +1,44 @@
-from PyQt6.QtWidgets import*
-from PyQt6.QtGui import*
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
 
-
-
-def show_window():
+def shop_window():
     window = QDialog()
+
     elements = [
         {
             "name": "test1",
-            "img": "hero.png",
-            "price": 100
+            "img":  "rocket.png",
+            "price": 100,
+            "name2": "test2",
+            "img2": "завантаження.png",
+            "price2": 200,
+            "name3": "test3",
+            "img3": "png-clipart-white-spaceship-rocket-rocket-photography-spacecraft.png",
+            "price3": 300
+
+
         }
     ]
 
     main_line = QHBoxLayout()
     for element in elements:
         ver = QVBoxLayout()
-        name_lb = QLabel(element["name"])
-        img_lb = QLabel()
-        img_pm = QPixmap(element["ing"])
+        name_lbl = QLabel(element["name"])
+        img_lbl = QLabel()
+        img_pm = QPixmap(element["img"])
         img_pm = img_pm.scaledToWidth(100)
-        img_lb.setPixmap(img_pm)
-        price_lb = QLabel(str(element["price"]))
+        img_lbl.setPixmap(img_pm)
+        price_lbl = QLabel(str(element["price"]))
         buy_btn = QPushButton("Купити")
-        ver.addWidget(name_lb)
-        ver.addWidget(img_lb)
-        ver.addWidget(price_lb)
+        ver.addWidget(name_lbl)
+        ver.addWidget(img_lbl)
+        ver.addWidget(price_lbl)
         ver.addWidget(buy_btn)
-        main_line.addLayout(main_line)
+        main_line.addLayout(ver)
 
 
+
+
+    window.setLayout(main_line)
     window.show()
     window.exec()
